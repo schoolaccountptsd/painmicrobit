b9940968798bf554a6d19647e4a2dfe533533fd5
schoolaccountptsd <141880896+schoolaccountptsd@users.noreply.github.com>
2023-11-17 09:31:08 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,5 +3,5 @@ input.onButtonPressed(Button.A, function () {
 })
 hummingbird.startHummingbird()
 basic.forever(function () {
-    hummingbird.setLED(ThreePort.One, hummingbird.getSensor(SensorType.Distance, ThreePort.One) / 2)
+	
 })
