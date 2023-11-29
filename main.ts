78e88c47d5e1021f275baeb79a0dac70d639cd4d
schoolaccountptsd <141880896+schoolaccountptsd@users.noreply.github.com>
2023-11-29 08:27:43 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,13 @@ input.onButtonPressed(Button.A, function () {
 	
 })
 hummingbird.startHummingbird()
+let rotation = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        rotation += 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        rotation += -1
+    }
+    hummingbird.setRotationServo(FourPort.One, rotation)
 })
